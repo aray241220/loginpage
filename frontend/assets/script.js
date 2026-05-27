4b2fc1a9b989f6cf126e253f2a9fedff4def4382
aray241220 <rayadrija2003@gmail.com>
2026-05-27 20:28:36 +0530

    Change API_URL to production endpoint

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://loginpage-three-liard.vercel.app/';
 
 function toggleForms() {
     const loginForm = document.getElementById('loginForm');
